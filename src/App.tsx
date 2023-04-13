e842ec9f97a2dc5a2496a7dc95762d4c5e675fec
Ryomi-j <ryomi.j78@gmail.com>
2023-04-13 16:11:17 +0900

    prepare to make frame

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+function App() {
+	return (
+		<>
+			준비중...
+			<Header />
+			<Footer />
+		</>
+	);
+}
+
+export default App;
