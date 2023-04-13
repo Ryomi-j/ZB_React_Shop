a9e15bb75453e8496f455206678e301f1ce780a7
Ryomi-j <ryomi.j78@gmail.com>
2023-04-13 21:57:51 +0900

    add zerobase link and card images to footer section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,38 @@
-const Footer = () => {
-    return <div></div>
-}
+import styled from "@emotion/styled";
+import { Link } from "react-router-dom";
+import { ReactComponent as Visa } from "../assets/cards/visa.svg";
+import { ReactComponent as Master } from "../assets/cards/master.svg";
+import { ReactComponent as Amax } from "../assets/cards/amax.svg";
+import { ReactComponent as Paypal } from "../assets/cards/paypal.svg";
+import { ReactComponent as Diners } from "../assets/cards/diners_club.svg";
+import { ReactComponent as Discover } from "../assets/cards/discover.svg";
 
-export default Footer
+const Footer = () => {
+	let cards = [Visa, Master, Amax, Paypal, Diners, Discover];
+
+	return (
+		<FooterContainer>
+			<Link to={"https://zero-base.co.kr/"}>제로베이스</Link>
+			<CardList>
+				
+					{cards.map((Card, key) => (
+						<li><Card key={key} /></li>
+					))}
+				
+			</CardList>
+		</FooterContainer>
+	);
+};
+
+const FooterContainer = styled.footer`
+	display: flex;
+	flex-direction: column;
+`;
+
+const CardList = styled.ul`
+	display: flex;
+	gap: 10px;
+`
+
+
+export default Footer;
