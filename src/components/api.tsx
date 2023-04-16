92a9919b2e6709a8034cda895398e329bdeee697
Ryomi-j <ryomi.j78@gmail.com>
2023-04-16 14:25:39 +0900

    get data from api

---
+++ src/components/api.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+
+interface RatingProps {
+	rate: number;
+	count: number;
+}
+
+interface DataProps {
+	id: number;
+	title: string;
+	price: number;
+	category: string;
+	description: string;
+	image: string;
+	reating: RatingProps;
+}
+
+const GetData = (url: string) => {
+	const [data, setData] = useState<DataProps[]>([]);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await axios.get<DataProps[]>(url);
+				setData(response.data);
+			} catch (err) {
+				console.error(err);
+			}
+		};
+		fetchData();
+	}, [url]);
+
+	return data;
+};
+
+export default GetData;
