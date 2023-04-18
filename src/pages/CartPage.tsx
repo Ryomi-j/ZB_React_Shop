255978bbca1815d11fe2a5bcb54aa61285069749
Ryomi-j <ryomi.j78@gmail.com>
2023-04-19 01:39:49 +0900

    make cart page ui and add item function

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,15 +1,70 @@
 import styled from "@emotion/styled";
 import ButtonItem from "../components/common/Button";
 import CartData from "../components/GetItem";
+import { CartItems } from "../App";
+import GetData from "../components/Api";
+import { DataProps } from "./ProductDetailPage";
+import HandleCart from "../components/HandleCart";
+import { AiOutlineMinusCircle } from "react-icons/ai";
+import { AiOutlinePlusCircle } from "react-icons/ai";
 
 interface CartItem {
 	id: number;
 	count: number;
 }
 
-const CartPage = () => {
-	let cart = CartData()
-	console.log(cart)
+const CartPage = ({ setCartItem }: { setCartItem: React.Dispatch<React.SetStateAction<CartItems>> }) => {
+	const cart = Object.values(CartData());
+	const apiData = GetData("https://fakestoreapi.com/products");
+
+	let cartItem: Array<DataProps> = [];
+	for (let i = 0; i < cart.length; i++) {
+		apiData.map((el, idx) => {
+			if (el.id === cart[i].id) {
+				cartItem.push(el);
+			}
+		});
+	}
+	console.log(cart);
+
+	const addOrSub = (product: DataProps, operator: string) => {
+		HandleCart(product.id, 1);
+		if (operator === "+") {
+			setCartItem((prev) => {
+				const count = prev.cartItem[product.id]?.count ?? 0;
+				return {
+					...prev,
+					cartCount: prev.cartCount + 1,
+					cartItem: {
+						...prev.cartItem,
+						[product.id]: {
+							id: product.id,
+							count: count + 1,
+						},
+					},
+				};
+			});
+		}
+
+		if (operator === "-") {
+			HandleCart(product.id, -1);
+			setCartItem((prev) => {
+				const count = prev.cartItem[product.id]?.count ?? 0;
+				return {
+					...prev,
+					cartCount: prev.cartCount - 1,
+					cartItem: {
+						...prev.cartItem,
+						[product.id]: {
+							id: product.id,
+							count: count - 1,
+						},
+					},
+				};
+			});
+		}
+	};
+
 	return (
 		<ContainerWrapper>
 			<Container>
@@ -17,12 +72,38 @@ const CartPage = () => {
 					<p>홈</p>
 					<span>장바구니</span>
 				</BreadCrumble>
-				<EmptyCartWrapper>
-					<h1>장바구니에 물품이 없습니다.</h1>
-					<ButtonItem content="담으러 가기" linkPage="/" />
-				</EmptyCartWrapper>
-
-				<ButtonItem content="구매하기" />
+				<CartItemContainerWrapper>
+					{cartItem.length > 0 ? (
+						<>
+							{cartItem.map((el: DataProps, idx) => {
+								return (
+									<CartItemContainer key={el.id}>
+										<ImageContainer>
+											<img src={el.image} alt={el.title} />
+										</ImageContainer>
+										<ProductDetailContainer>
+											<h2>
+												<a>{el.title}</a>
+											</h2>
+											<p>{el.price}</p>
+											<ButtonWrapper>
+												<ButtonItem icon={AiOutlineMinusCircle} handleClick={() => addOrSub(el, "-")} />
+												<p>{cart[idx].count}</p>
+												<ButtonItem icon={AiOutlinePlusCircle} handleClick={() => addOrSub(el, "+")} />
+											</ButtonWrapper>
+										</ProductDetailContainer>
+									</CartItemContainer>
+								);
+							})}
+						</>
+					) : (
+						<EmptyCartWrapper>
+							<h1>장바구니에 물품이 없습니다.</h1>
+							<ButtonItem content="담으러 가기" linkPage="/" />
+						</EmptyCartWrapper>
+					)}
+					<ButtonItem content="구매하기" />
+				</CartItemContainerWrapper>
 			</Container>
 		</ContainerWrapper>
 	);
@@ -72,6 +153,74 @@ const BreadCrumble = styled.div`
 			border-right: 1px solid;
 			transform: translate(-50%, -50%) rotate(45deg);
 		}
+	}
+`;
+
+const CartItemContainerWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	padding-top: 3.5rem;
+	width: 100%
+	color: #1f2937;
+`;
+
+const CartItemContainer = styled.div`
+	display: flex;
+	margin: 1rem auto;
+	width: 60rem;
+	height: 15.5rem;
+`;
+
+const ImageContainer = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	padding: 1rem;
+	width: 14rem;
+
+	& > img {
+		max-width: 100%;
+		max-height: 100%;
+	}
+`;
+
+const ProductDetailContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	padding: 2rem 3rem;
+
+	& h2 {
+		font-size: 1.3rem;
+		font-weight: bold;
+		line-height: 2rem;
+	}
+
+	& p {
+		margin: 3rem 0 1rem;
+		font-size: 1.875rem;
+	}
+`;
+
+const ButtonWrapper = styled.div`
+	display: flex;
+	padding-top: 1rem;
+	margin-left: 24rem;
+	width: 10rem;
+	text-align: center;
+
+	& > button {
+		justify-content: center;
+		font-size: 1.5rem;
+		padding: 0;
+		width: 5rem;
+
+		svg {
+			width: 5rem;
+		}
+	}
+
+	& p {
+		margin: auto;
 	}
 `;
 
